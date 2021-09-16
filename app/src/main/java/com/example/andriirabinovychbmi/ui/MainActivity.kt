f03b06360a2c515e9fb34aefddbe45b3590f4813
AndriiR <japonchik0@gmail.com>
2021-09-16 11:47:40 +0300

    Implemented view binding in MainActivity.kt

--- app/src/main/java/com/example/andriirabinovychbmi/ui/MainActivity.kt
+++ app/src/main/java/com/example/andriirabinovychbmi/ui/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.andriirabinovychbmi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.andriirabinovychbmi.R
+import com.example.andriirabinovychbmi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.fragment_details_bmi) {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_details_bmi)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
