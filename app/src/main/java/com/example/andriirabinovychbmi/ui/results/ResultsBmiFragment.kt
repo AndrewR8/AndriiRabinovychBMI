e4c9e0f524ddb0dbcc3503c05ce2f4a6e8206af3
AndriiR <japonchik0@gmail.com>
2021-09-16 17:34:57 +0300

    Updated ResultsBmiFragment.kt

--- app/src/main/java/com/example/andriirabinovychbmi/ui/results/ResultsBmiFragment.kt
+++ app/src/main/java/com/example/andriirabinovychbmi/ui/results/ResultsBmiFragment.kt
@@ -1,8 +1,36 @@
 package com.example.andriirabinovychbmi.ui.results
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.andriirabinovychbmi.R
+import com.example.andriirabinovychbmi.databinding.FragmentResultsBinding
+import com.example.andriirabinovychbmi.ui.details.DetailsBmiFragment
+import com.example.andriirabinovychbmi.ui.details.DetailsBmiViewModel
 
-class ResultsBmiFragment : Fragment(R.layout.fragment_results){
+class ResultsBmiFragment : Fragment(){
 
+    private lateinit var binding: FragmentResultsBinding
+
+    private lateinit var viewModel: ResultsBmiViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        viewModel = ViewModelProvider(this).get(ResultsBmiViewModel::class.java)
+        return inflater.inflate(R.layout.fragment_results, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentResultsBinding.bind(view)
+
+    }
 }
